493d89e7a1ffb3e410fca4761284da493fe74921
Jay Conrod <jayconrod@google.com>
2017-10-26 14:39:40 -0400

    packages.Walk calls back in all directories

--- go/tools/gazelle/rules/generator_test.go
+++ go/tools/gazelle/rules/generator_test.go
@@ -33,6 +33,7 @@ import (
 func testConfig(repoRoot, goPrefix string) *config.Config {
 	c := &config.Config{
 		RepoRoot:            repoRoot,
+		Dirs:                []string{repoRoot},
 		GoPrefix:            goPrefix,
 		GenericTags:         config.BuildTags{},
 		Platforms:           config.DefaultPlatformTags,
@@ -45,8 +46,8 @@ func testConfig(repoRoot, goPrefix string) *config.Config {
 func packageFromDir(c *config.Config, dir string) (*packages.Package, *bf.File) {
 	var pkg *packages.Package
 	var oldFile *bf.File
-	packages.Walk(c, dir, func(_ *config.Config, p *packages.Package, f *bf.File) {
-		if p.Dir == dir {
+	packages.Walk(c, dir, func(rel string, _ *config.Config, p *packages.Package, f *bf.File, _ bool) {
+		if p != nil && p.Dir == dir {
 			pkg = p
 			oldFile = f
 		}
